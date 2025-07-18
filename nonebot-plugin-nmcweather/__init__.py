9449edb5acea84f6b551d545d2e95492654e3364
kjsjs <3226298732@qq.com>
2025-07-18 09:49:27 +0800

    init

---
+++ nonebot-plugin-nmcweather/__init__.py
@@ -0,0 +1,12 @@
+from nonebot.plugin import PluginMetadata
+from .weather import weather
+
+__plugin_meta__ = PluginMetadata(
+    name="天气查询",
+    description="通过NMC API查询实时天气",
+    usage="天气 省份-区县|天气 区县|支持区县 省份",
+    type="application",
+    homepage="{项目主页}",
+    version="0.1.0",
+    supported_adapters={"~onebot.v11"},
+)
